2111961db01f9189bc5d73287bfe2d56810f2ee1
Aleksandr <104426761+nemirovka28@users.noreply.github.com>
2022-10-25 16:00:49 +0300

    f

--- js/index.js
+++ js/index.js
@@ -6,20 +6,31 @@ myButton.addEventListener('click', onClick);
 function onClick(ev) {
   ev.preventDefault();
   console.log('click');
-  let name = document.forms['person_information'].elements['person_name'].value;
-  let tel = document.forms['person_information'].elements['person_telephone'].value;
+  // let name = document.forms['print_information'].elements['person_name'].value;
+  // let email = document.forms['print_informationEmail'].elements['person_email'].value;
+  // let tel = document.forms['print_informationTel'].elements['phone'].value;
+  const iti = document.querySelector('.iti__selected-flag');
+  console.log(iti.getAttribute('title'));
+
+  let name = document.querySelector('.print_information').value;
+  let email = document.querySelector('.print_informationEmail').value;
+  let tel = document.querySelector('.print_informationTel').value;
+
   console.log('Name', name);
   console.log('Telephone', tel);
+  console.log('Email', email);
   let obj = {
     name: name,
+    email: email,
     tel: tel,
   };
-  fetch('/add', { method: 'POST', body: JSON.stringify(obj), headers: { 'content-type': 'application/json' } })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      alert(data);
-    })
-    .catch(alert);
+  console.log(obj);
+  // fetch('/add', { method: 'POST', body: JSON.stringify(obj), headers: { 'content-type': 'application/json' } })
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     alert(data);
+  //   })
+  //   .catch(alert);
 }
---
+++ js/phoneMask.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var phones = document.querySelectorAll('#phone');
+  for (var i = phones.length - 1; i >= 0; i--) {
+    processInput(phones[i]);
+  }
+});
+
+function processInput(phone) {
+  var iti = window.intlTelInput(phone, {
+    allowDropdown: true,
+    nationalMode: false,
+    autoPlaceholder: 'aggressive',
+    formatOnDisplay: true,
+    separateDialCode: true,
+    initialCountry: 'auto',
+    preferredCountries: ['ru', 'by', 'kz'],
+    onlyCountries: [
+      'al',
+      'ad',
+      'at',
+      'by',
+      'be',
+      'ba',
+      'bg',
+      'hr',
+      'cz',
+      'dk',
+      'ee',
+      'fo',
+      'fi',
+      'fr',
+      'de',
+      'gi',
+      'gr',
+      'va',
+      'hu',
+      'is',
+      'ie',
+      'it',
+      'lv',
+      'li',
+      'lt',
+      'lu',
+      'mk',
+      'mt',
+      'md',
+      'mc',
+      'me',
+      'nl',
+      'no',
+      'pl',
+      'pt',
+      'ro',
+      'ru',
+      'sm',
+      'rs',
+      'sk',
+      'si',
+      'es',
+      'se',
+      'ch',
+      'az',
+      'am',
+      'gb',
+      'tr',
+      'cy',
+      'ge',
+      'kz',
+      'us',
+    ],
+    geoIpLookup: function (callback) {
+      $.get('https://ipinfo.io', function () {}, 'json').always(function (resp) {
+        var countryCode = resp && resp.country ? resp.country : 'us';
+        callback(countryCode);
+        return countryCode;
+      });
+    },
+    utilsScript: './intl-tel-input-17.0.18/build/js/ajax-geo.js',
+  });
+
+  phone.addEventListener('input', () => {
+    phone.setCustomValidity('');
+    if (!iti.isValidNumber()) phone.setCustomValidity('!!!');
+  });
+}
+setTimeout(function () {
+  var link = $('.mask').attr('placeholder');
+
+  setTimeout(function () {
+    document.querySelector('.first_code').innerHTML = link;
+
+    $(function () {
+      $('.mask').mask(link);
+    });
+  }, 50);
+}, 5000);
+
+$('form').hover(function () {
+  $('form').removeClass('hover');
+  $(this).addClass('hover');
+
+  $('body').on('click touchend', '.iti__country', function (e) {
+    setTimeout(function () {
+      var link_new = $('.hover .mask').attr('placeholder');
+      var link_end = (document.querySelector('.second_code').innerHTML = link_new);
+
+      $(function () {
+        $('.hover .mask').mask(link_end);
+      });
+
+      setTimeout(function () {
+        document.querySelector('.first_code').innerHTML = link_end;
+      }, 50);
+    }, 300);
+  });
+});
